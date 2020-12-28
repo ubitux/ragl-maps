2065cbfbe8fbb8f850253db816b77ed743f3aa03
Clément Bœsch <u@pkh.me>
2020-12-28 13:39:31 +0100

    Add briefing stuff

--- scripts/sync-ref-data.sh
+++ scripts/sync-ref-data.sh
@@ -4,4 +4,5 @@ for map in basemaps/*; do
 	for refdata in ref-data/*; do
 		cp -v $refdata/* $map
 	done
+	python scripts/map-brief-gen.py ragl-s10-briefing.txt > $map/ragl-briefing-rules.yaml
 done
