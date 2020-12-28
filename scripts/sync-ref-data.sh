62e505874fd9c3cb11977ad1a88e3e65f44b2253
Clément Bœsch <u@pkh.me>
2020-12-28 13:48:39 +0100

    Add basic packaging (need cleanup)

--- scripts/sync-ref-data.sh
+++ scripts/sync-ref-data.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+rm -rf oramap
+mkdir -p oramap
 for map in basemaps/*; do
 	for refdata in ref-data/*; do
 		cp -v $refdata/* $map
 	done
 	python scripts/map-brief-gen.py ragl-s10-briefing.txt > $map/ragl-briefing-rules.yaml
+	(cd $map && zip -9 -r ../../oramap/$(basename $map).oramap .)
 done
